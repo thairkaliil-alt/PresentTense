d9085b94d35633f782767cd2630a48c6e0339d13
thairkaliil-alt <thair.kaliil@gmail.com>
2026-07-08 23:07:10 +0300

    Update MainActivity.kt

--- app/src/main/java/com/allinone/blocker/ui/MainActivity.kt
+++ app/src/main/java/com/allinone/blocker/ui/MainActivity.kt
@@ -47,6 +47,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
