ca52446272988c56f133bb7aeb5583db96ccc42c
thairkaliil-alt <thair.kaliil@gmail.com>
2026-07-12 01:20:22 +0300

    Update PermissionsScreen.kt

--- app/src/main/java/com/allinone/blocker/ui/PermissionsScreen.kt
+++ app/src/main/java/com/allinone/blocker/ui/PermissionsScreen.kt
@@ -28,7 +28,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
